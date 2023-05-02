194bbf208ab7983d81257896ec23dc7646f54a68
hyemin <88538911+hhyem@users.noreply.github.com>
2023-05-02 18:59:04 +0900

    activity 생성

---
+++ app/src/main/java/com/example/appproject/app_write.kt
@@ -0,0 +1,11 @@
+package com.example.appproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class app_write : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_write)
+    }
+}
